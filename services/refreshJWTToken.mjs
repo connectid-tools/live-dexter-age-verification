c06e4a57a0a9fc104e630b045a462fda16b814d8
Dan Braine <danbraine@192-168-1-100.tpgi.com.au>
2024-09-04 20:30:44 +1000

    update jwt token

--- services/refreshJWTToken.mjs
+++ services/refreshJWTToken.mjs
@@ -1,5 +1,3 @@
-// jwtService.mjs
-
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
@@ -8,13 +6,16 @@ dotenv.config();
 
 // Function to refresh JWT Token
 export async function refreshJWTToken() {
+
   try {
     console.log('Refreshing JWT token...');
     const store_hash = process.env.STORE_HASH;
     const storeDomain = process.env.STORE_DOMAIN;
+    const endpointDomain = process.env.ENDPOINT_DOMAIN;
+    const accessToken = process.env.ACCESS_TOKEN;
 
-    if (!store_hash || !storeDomain) {
-      throw new Error('Missing required environment variables: STORE_HASH or STORE_DOMAIN');
+    if (!store_hash || !storeDomain || !endpointDomain || !accessToken) {
+      throw new Error('Missing required environment variables: STORE_HASH, STORE_DOMAIN, ENDPOINT_DOMAIN, or ACCESS_TOKEN');
     }
 
     const url = `https://api.bigcommerce.com/stores/${store_hash}/v3/storefront/api-token`;
@@ -24,19 +25,24 @@ export async function refreshJWTToken() {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        'X-Auth-Token': process.env.ACCESS_TOKEN,
+        'X-Auth-Token': accessToken,
       },
       body: JSON.stringify({
-        allowed_cors_origins: [`https://${storeDomain}.mybigcommerce.com/`],
+        allowed_cors_origins: [
+          `https://${storeDomain}.mybigcommerce.com`,
+          `https://${endpointDomain}.ondigitalocean.app`
+        ],
         channel_id: 1,
-        expires_at: Math.floor(Date.now() / 1000) + 3600,
+        expires_at: Math.floor(Date.now() / 1000) + 3600,  // Expire in one hour
       }),
     };
 
     const response = await fetch(url, options);
 
     if (!response.ok) {
-      throw new Error(`Failed to refresh JWT token: ${response.statusText}`);
+      const errorResponse = await response.text();
+      console.error('Failed to refresh JWT token:', response.statusText, errorResponse);
+      throw new Error(`Failed to refresh JWT token: ${response.statusText} - ${errorResponse}`);
     }
 
     const data = await response.json();
