25f3fcfb1a3b6ababd68d36fcf5753fb91b3e243
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-09-10 11:03:25 +1000

    add all verification logic in app.mjs and remove routes

--- app.mjs
+++ app.mjs
@@ -7,22 +7,15 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import createError from 'http-errors';
 import cors from 'cors';
+import https from 'https';
+import { Buffer } from 'buffer';
 import RelyingPartyClientSdk from '@connectid-tools/rp-nodejs-sdk';
 import { config } from './config.js';
 
 const rpClient = new RelyingPartyClientSdk(config);
 
-// Import routes
-import indexRouter from './routes/index.mjs';
-import validateCartRouter from './routes/restrictItems.mjs';
-import getRestrictedItemsRouter from './routes/getRestrictedItems.mjs';
-import participantsRouter from './routes/participants.mjs';
-import selectBankRouter from './routes/select-bank.mjs';
-import retrieveTokensRouter from './routes/retrieve-tokens.mjs';
-
 // Initialize Express app
 const app = express();
-const port = 3001;
 
 // Use CORS middleware
 const storeDomain = process.env.STORE_DOMAIN;
@@ -50,10 +43,80 @@ app.use('/', indexRouter);
 app.use('/validate-cart', validateCartRouter);
 app.use('/restricted-items', getRestrictedItemsRouter);
 
-// Add the new routes
-app.use('/participants', participantsRouter(rpClient));
-app.use('/select-bank', selectBankRouter(rpClient));
-app.use('/retrieve-tokens', retrieveTokensRouter(rpClient));
+// Handle the user's bank selection and start the OIDC flow
+app.post('/select-bank', async (req, res) => {
+  const essentialClaims = req.body.essentialClaims || [];
+  const voluntaryClaims = req.body.voluntaryClaims || [];
+  const purpose = req.body.purpose || config.data.purpose;
+  const authServerId = req.body.authorisationServerId;
+
+  if (!authServerId) {
+    const error = 'authorisationServerId parameter is required';
+    console.error(error);
+    return res.status(400).json({ error });
+  }
+
+  try {
+    console.log(
+      `Processing request to send PAR with authorisationServerId='${authServerId}' essentialClaims='${essentialClaims.join(
+        ','
+      )}' voluntaryClaims='${voluntaryClaims.join(',')}', purpose='${purpose}'`
+    );
+
+    const { authUrl, code_verifier, state, nonce, xFapiInteractionId } = await rpClient.sendPushedAuthorisationRequest(
+      authServerId,
+      essentialClaims,
+      voluntaryClaims,
+      purpose
+    );
+
+    res.cookie('state', state, { path: '/', sameSite: 'none', secure: true });
+    res.cookie('nonce', nonce, { path: '/', sameSite: 'none', secure: true });
+    res.cookie('code_verifier', code_verifier, { path: '/', sameSite: 'none', secure: true });
+    res.cookie('authorisation_server_id', authServerId, { path: '/', sameSite: 'none', secure: true });
+
+    console.log(`PAR sent to authorisationServerId='${authServerId}', returning url='${authUrl}', x-fapi-interaction-id='${xFapiInteractionId}'`);
+
+    return res.json({ authUrl });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.toString() });
+  }
+});
+
+// Following successful authentication and consent at the bank, exchange the auth token for an ID Token
+app.get('/retrieve-tokens', async (req, res) => {
+  if (!req.query.code) {
+    console.error('No code parameter in query string');
+    return res.status(400).json({ error: 'No code parameter in query string' });
+  }
+
+  try {
+    const tokenSet = await rpClient.retrieveTokens(
+      req.cookies.authorisation_server_id,
+      req.query,
+      req.cookies.code_verifier,
+      req.cookies.state,
+      req.cookies.nonce
+    );
+
+    const claims = tokenSet.claims();
+    const token = {
+      decoded: JSON.stringify(jwtDecode(tokenSet.id_token), null, 2),
+      raw: tokenSet.id_token,
+    };
+
+    console.log(`Returned claims: ${JSON.stringify(claims, null, 2)}`);
+    console.log(`Returned raw id_token: ${token.raw}`);
+    console.log(`Returned decoded id_token: ${token.decoded}`);
+    console.log(`Returned xFapiInteractionId: ${tokenSet.xFapiInteractionId}`);
+
+    return res.json({ claims, token, xFapiInteractionId: tokenSet.xFapiInteractionId });
+  } catch (error) {
+    console.error('Error retrieving tokenset: ' + error);
+    return res.status(500).json({ error: error.toString() });
+  }
+});
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -69,9 +132,12 @@ app.use(function(err, req, res, next) {
   res.status(statusCode).render('error', { error: err });
 });
 
-// Start the Express server
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+// Set up HTTPS server with the key and certificate from the environment variables
+const key = Buffer.from(config.data.transport_key_content, 'base64');
+const cert = Buffer.from(config.data.transport_pem_content, 'base64');
+
+https.createServer({ key, cert }, app).listen(config.data.server_port, config.data.listen_address, () => {
+  console.log(`Server is listening on ${config.data.listen_address} on port ${config.data.server_port}`);
 });
 
 export default app;
