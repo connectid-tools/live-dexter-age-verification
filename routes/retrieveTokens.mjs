114ee51d380114c1de205c0e21dc7518ed07a096
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-09-16 13:12:00 +1000

    update checkout essential claims

--- routes/retrieveTokens.mjs
+++ routes/retrieveTokens.mjs
@@ -2,7 +2,6 @@ import express from 'express';
 import RelyingPartyClientSdk from '@connectid-tools/rp-nodejs-sdk';
 import jwtDecode from 'jwt-decode';
 import { config } from '../config.js';
-import console from '../console.js'; // Assuming you have a console module
 
 const router = express.Router();
 const rpClient = new RelyingPartyClientSdk(config);
