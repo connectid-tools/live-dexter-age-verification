2a67e30e0e10d706d01e52422e3aab5511aa5d62
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-09-18 12:44:25 +1000

    update select and token retrieval logic

--- utils/cookieUtils.mjs
+++ utils/cookieUtils.mjs
@@ -1,14 +1,14 @@
-
 import express from 'express';
 const router = express.Router();
 
-
+// Named export for clearCookies function
 export const clearCookies = (res, logger) => {
     logger.info('Clearing all cookies');
     res.clearCookie('state');
     res.clearCookie('nonce');
     res.clearCookie('code_verifier');
     res.clearCookie('authorisation_server_id');
-  };
+};
 
-  export default router;
+// Default export for the router
+export default router;
