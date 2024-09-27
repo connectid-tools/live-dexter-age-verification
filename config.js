ff860a74123178becde88010f5f55569154ae683
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-09-27 12:03:04 +1000

    remove getLogs and tokenManager

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ export const config = {
 
     // registry_participants_uri: 'https://api.sandbox.connectid.com.au/oidf-conformance/participants?alias=a/sheldonandhammond',
     registry_participants_uri: 'https://data.directory.sandbox.connectid.com.au/participants',
-    include_uncertified_participants: true,
+    include_uncertified_participants: false,
 
     purpose: 'Your details will be shared for the purpose of verifying you are over 18 to prevent sale of knives to minors this information is shared with Sheldon and Hammond',
 
