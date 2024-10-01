40237801022faad0f6318c62e496a344cb52bc2d
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-10-01 10:23:28 +1000

    add corsOptions

--- app.mjs
+++ app.mjs
@@ -4,7 +4,7 @@ dotenv.config();
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
-import cors, { setCorsHeaders, corsOptions } from './middleware/cors.mjs';
+import cors, { corsOptions } from './middleware/cors.mjs';
 import { notFoundHandler, errorHandler } from './middleware/errorHandler.mjs';
 import indexRouter from './routes/index.mjs';
 import validateCartRouter from './routes/restrictItems.mjs';
@@ -25,7 +25,6 @@ app.use(express.static(path.join(path.resolve(), 'public')));
 
 // Apply CORS middleware with options before routes
 app.use(cors(corsOptions));      // <-- Ensure CORS is applied here
-app.use(setCorsHeaders);
 app.use(cookieParser());
 
 // Routes
