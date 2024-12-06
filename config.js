7ffbbc393df9dab25aa5d2bcfe8731f2a11aa748
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-12-06 11:00:56 +1100

    update config with production urls

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ export const config = {
     listen_address: '0.0.0.0',
     log_level: 'debug',
     application_redirect_uri: `https://${process.env.STORE_DOMAIN}/checkout`,
-    registry_participants_uri: 'https://data.directory.sandbox.connectid.com.au/participants',  
+    registry_participants_uri: 'https://data.directory.connectid.com.au/participants',  
     purpose: 'verifying you are over 18',
     include_uncertified_participants: false,
     enable_auto_compliance_verification: false,
