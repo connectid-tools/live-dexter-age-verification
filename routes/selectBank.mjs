2a54d6de905c57db9dc5e521b26d4037783be661
Daniel Braine <danielb@sheldonandhammond.com.au>
2024-12-11 12:29:26 +1100

    use encrypted JWT

--- routes/selectBank.mjs
+++ routes/selectBank.mjs
@@ -10,7 +10,7 @@ const logger = getLogger('info');
 const router = express.Router();
 const rpClient = new RelyingPartyClientSdk(config);
 
-const ENCRYPTION_SECRET = Buffer.from('4beced985ddf9a778fc9e4656e315ce9c5bb645a3c5ba6887391fd469a74ce32', 'hex');
+const ENCRYPTION_SECRET = '4beced985ddf9a778fc9e4656e315ce9c5bb645a3c5ba6887391fd469a74ce32';
 if (ENCRYPTION_SECRET.length !== 32) {
     throw new Error(`Invalid encryption key length: ${ENCRYPTION_SECRET.length * 8} bits. Expected 256 bits.`);
 }
